6113112ed2a185ef72abd5a32fe5ddc6bac0e200
avdosev <avdosev1@yandex.ru>
2020-09-13 13:23:55 +0400

    Add test

--- test/either_test.dart
+++ test/either_test.dart
@@ -6,5 +6,12 @@ void main() {
   test('right generic is right', () {
     final maybe = Right<String, bool>(true);
     expect(maybe.isRight, true);
+    expect(maybe.isLeft, false);
+  });
+
+  test('left generic is left', () {
+    final maybe = Left<String, bool>("true");
+    expect(maybe.isLeft, true);
+    expect(maybe.isRight, false);
   });
 }
