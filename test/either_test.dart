6b9ecf94b05e1d28aa081c494dfa3c072f0ad8ca
avdosev <avdosev1@yandex.ru>
2020-09-15 11:35:43 +0400

    tests

--- test/either_test.dart
+++ test/either_test.dart
@@ -14,4 +14,44 @@ void main() {
     expect(maybe.isLeft, true);
     expect(maybe.isRight, false);
   });
+
+  test('map right', () {
+    final maybe = Right<String, bool>(true);
+    maybe.either(
+        (left) => expect(false, true), 
+        (right) => expect(right, true));
+    
+    maybe
+      .map((right) => false)
+      .either(
+        (left) => expect(false, true), 
+        (right) => expect(right, false));
+
+    maybe.either(
+        (left) => expect(false, true), 
+        (right) => expect(right, true));
+  });
+
+  test('map left', () {
+    final maybe = Left<bool, String>(true);
+    maybe.either(
+        (left) => expect(left, true), 
+        (right) => expect(false, true));
+    
+    maybe
+      .map((right) => false)
+      .either(
+        (left) => expect(left, true), 
+        (right) => expect(true, false));;
+  });
+
+  test('unite', () {
+    expect(Left<String, String>("").unite<bool>((left) => true, (right) => false), true);
+    expect(Right<String, String>("").unite<bool>((left) => true, (right) => false), false);
+  });
+
+  test('cond', () {
+    expect(Either.cond(true, "right", "left").isRight, true);
+    expect(Either.cond(false, "right", "left").isLeft, true);
+  });
 }
